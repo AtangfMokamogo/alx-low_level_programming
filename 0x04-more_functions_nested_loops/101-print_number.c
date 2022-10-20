9e7f4522e3db115cf072b6c281b6fcb2f4469f48
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-10-20 09:08:13 -0700

    added advanced tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	int x = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		x = -x;
+	}
+
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+
+	putchar((x % 10) + '0');
+}
