94b5d4c632e195a929c84f611c00978d3b0dcc2a
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-10-24 11:15:22 -0700

    Added file 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string.
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		putchar(*str);
+		*str++;
+	}
+	putchar('\n');
+}
