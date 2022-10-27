cfd97f3e20088212880ed9e17f56ab772d2c34e0
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-10-27 12:09:54 -0700

    fixed eror on file 2-strncpy.c caused by linter failure

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,21 @@
 #include "main.h"
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
- *
- * Return: A pointer to the resulting string dest.
- */
+  * _strncpy - copies a string
+  * @dest: destination
+  * @src: source
+  * @n: amount of bytes from src
+  *
+  * Return: the pointer to dest
+  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int iterator = 0, len_src = 0;
+	int x;
 
-	while (src[iterator++])
-		len_src++;
-
-	for (iterator = 0; src[iterator] && iterator < n; iterator++)
-		dest[iterator] = src[iterator];
-
-	for (index = len_src; iterator < n; iterator++)
-		dest[iterator] = '\0';
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+	for ( ; x < n; x++)
+		dest[x] = '\0';
 
 	return (dest);
 }
