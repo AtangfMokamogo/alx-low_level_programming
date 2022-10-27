3423d10b2e50b6505c7944b82b6cf9cad25374ee
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-10-27 14:07:44 -0700

    fixed error on file 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,13 +4,13 @@
   * @a: The array of integers to be reversed.
   * @n: The number of elements in the array.
   */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int x, y, tmp;
 
-	for (x =0; x < n - 1; x++)
+	for (x = 0; x < n - 1; x++)
 	{
-		for (y = x + 1; y > 0; y --)
+		for (y = x + 1; y > 0; y--)
 		{
 			tmp = *(a + y);
 			*(a + y) = *(a + (y - 1));
