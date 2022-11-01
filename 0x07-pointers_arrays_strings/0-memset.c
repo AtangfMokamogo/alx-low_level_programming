76bf8f98aadaf3d17c79a15ad270228bc062b0f4
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-11-01 07:02:33 -0700

    This commit adds file 0-memset.c for task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _memset -fills memory with a constant byte
+  *
+  * @s: pointer to memory area to be filled
+  * @b: the constant byte
+  * @n: the number of bytes to fill
+  *
+  * Return: a pointer to memory area @s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		s{x} = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+a non empty readme. replace with task descriptions
