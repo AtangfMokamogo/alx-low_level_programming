fee10971064d83a2915f5c31969ba8ed0deed9f1
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-11-16 08:16:21 -0800

    added files for all mandatory tasks

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef function_pointers
+#define function_pointers
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers di         rectory.
+ */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
