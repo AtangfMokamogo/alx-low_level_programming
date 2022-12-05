a99a39f2755ac62030cca36eda3317b140521116
AtangFMokamogo <atangfmokamogo@outlook.com>
2022-12-05 08:10:13 -0800

    added new file to fix betty error on too many functions in one file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,9 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+#include "main.h"
 
 /**
- * print_addr - prints address
+ * _addr - prints address
  * @ptr: magic.
  * Return: no return.
  */
@@ -78,28 +79,6 @@ void print_type(char *ptr)
 		printf("CORE (Core file)\n");
 	else
 		printf("<unknown: %x>\n", type);
-}
-
-/**
- * print_osabi - prints osabi
- * @ptr: magic.
- * Return: no return.
- */
-void print_osabi(char *ptr)
-{
-	char osabi = ptr[7];
-
-	printf("  OS/ABI:                            ");
-	if (osabi == 0)
-		printf("UNIX - System V\n");
-	else if (osabi == 2)
-		printf("UNIX - NetBSD\n");
-	else if (osabi == 6)
-		printf("UNIX - Solaris\n");
-	else
-		printf("<unknown: %x>\n", osabi);
-
-	printf("  ABI Version:                       %d\n", ptr[8]);
 }
 
 
